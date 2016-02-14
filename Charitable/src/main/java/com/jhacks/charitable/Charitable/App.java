f1bba790465892a954a6cdf39e693f1fe53495ce
Elliot Linder <elliot.linder@rutgers.edu>
2016-02-14 01:52:46 -0500

    ZIP distance calculation

---
+++ Charitable/src/main/java/com/jhacks/charitable/Charitable/App.java
@@ -0,0 +1,9 @@
+package com.jhacks.charitable.Charitable;
+
+public class App 
+{
+    public static void main( String[] args ) throws Exception
+    {
+        MapsStuff.TwoPointsDistance("07666", "08901");
+    }
+}
