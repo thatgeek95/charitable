5e1a1e6d642193b933aca606f1c22a8e298faf4b
Joseph Kotzker <joeykotzker@gmail.com>
2016-02-14 11:16:07 -0500

    Got the GUI working
    Got the GUI working and the scenes linked to eachother

--- Joey/src/com/charitable/frontend/Main.java
+++ Joey/src/com/charitable/frontend/Main.java
@@ -1,25 +1,31 @@
 package com.charitable.frontend;
-	
+
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.AnchorPane;
 
 
 public class Main extends Application {
+	
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(getClass().getResource("UI-scene1.fxml"));
+
+			AnchorPane root = (AnchorPane)loader.load();
+			Scene scene = new Scene(root,651,475);
 			primaryStage.setScene(scene);
+			primaryStage.setTitle("charitABLE");
+			primaryStage.setResizable(false);
 			primaryStage.show();
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public static void main(String[] args) {
 		launch(args);
 	}
